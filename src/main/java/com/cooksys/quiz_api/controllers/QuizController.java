ef4896d43dbfc91a221e3f9d99adbf78c57d5d56
Matt Heerspink <mheerspink75@gmail.com>
2021-10-14 20:28:40 -0500

    added getRandomQuestion() to QuizController

--- src/main/java/com/cooksys/quiz_api/controllers/QuizController.java
+++ src/main/java/com/cooksys/quiz_api/controllers/QuizController.java
@@ -2,10 +2,12 @@ package com.cooksys.quiz_api.controllers;
 
 import java.util.List;
 
+import com.cooksys.quiz_api.dtos.QuestionResponseDto;
 import com.cooksys.quiz_api.dtos.QuizResponseDto;
 import com.cooksys.quiz_api.services.QuizService;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +26,10 @@ public class QuizController {
   }
   
   // TODO: Implement the remaining 6 endpoints from the documentation.
+
+  @GetMapping("/{id}/random")
+  public QuestionResponseDto getRandomQuestion(@PathVariable("id") Long quizID) {
+    return null;
+  }
 
 }
